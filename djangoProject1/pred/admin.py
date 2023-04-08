34355e5fea78fabd7d19655f66e37a90161270d0
trabelsiseif7 <63430881+trabelsiseif7@users.noreply.github.com>
2023-04-08 03:55:48 +0100

    .

--- djangoProject1/pred/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from pred.models import product, burreau, pc_gamer, smartphone
-
-# Register your models here.
-
-admin.site.register(product)
-admin.site.register(burreau)
-admin.site.register(pc_gamer)
-admin.site.register(smartphone)
